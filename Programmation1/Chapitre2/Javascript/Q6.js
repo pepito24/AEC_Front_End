a10a38bf3393928f72e2071ceb60627dc89b175c
pepito24 <erikpaquetweb@gmail.com>
2019-11-14 21:50:31 -0500

    exercice 6

--- Programmation1/Chapitre2/Javascript/Q6.js
+++ Programmation1/Chapitre2/Javascript/Q6.js
@@ -3,22 +3,22 @@ var note;
 
 note = Number(prompt("Ecrit un note sur 100"));
 
-if (note >= 90 ) {
+if (note >= 90 && note <= 100 ) {
     document.write("A")
 }
 
-else if (note >= 80 ) {
+else if (note >= 80 && note < 90 ) {
     document.write("B")
 }
 
-else if (note >= 70) {
+else if (note >= 70 && note < 80) {
     document.write("C")
 }
 
-else if (note >= 60) {
+else if (note >= 60 && note < 70 ) {
     document.write("D")
 }
 
-else if (note < 60) {
+else if (note < 60 && note >= 0 ) {
     document.write("E")
 }
