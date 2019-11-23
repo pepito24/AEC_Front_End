8c79d94ba6d2df845df5bd59f7fbf4cc34816d4c
pepito24 <erikpaquetweb@gmail.com>
2019-11-23 08:28:10 -0500

    Mise a jour 23 novembre

---
+++ Programmation1/Chapitre2/Javascript/Q15.js
@@ -0,0 +1,13 @@
+var perimetre;
+var surface;
+var largeur;
+var longueur;
+
+
+surface = largeur * longueur;
+
+perimetre = (largeur + longueur) * 2;
+
+if (){
+
+}
--- Programmation1/Chapitre2/Javascript/Q5.js
+++ Programmation1/Chapitre2/Javascript/Q5.js
@@ -1,12 +1,13 @@
 var temperature;
 
-temperature = String(prompt("Ecrit la température extérieur"));
+temperature = Number(prompt("Ecrit la température extérieur"));
 
-if (temperature <= "-40" ) {
-    if (temperature >= "-10") {
+if (temperature >= -40 && temperature <= -10  ) {
         document.write("HAAAAAAAAA! Il fait froid")
-    }
+}
 
+else if (temperature <= -40 ){
+    document.write("Cest pas humain")
 }
 
 else {
