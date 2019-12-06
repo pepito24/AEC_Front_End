8bc1d1e841509bdeb43986e9eab96f2d1f0fce58
pepito24 <erikpaquetweb@gmail.com>
2019-12-05 21:01:54 -0500

    Fin examen

--- Programmation1/Chapitre2/Examen1/Question3.js
+++ Programmation1/Chapitre2/Examen1/Question3.js
@@ -7,13 +7,11 @@ var sauce;
 var sauceTexte;
 var nbPersonnes;
 var prix = 0;
-var prixTotal = 0;
 var taxesTotal = (0.05 + 0.09975);
 
 legume = prompt("Voulez-vous des légumes : oui ou non");
 pepperoni = prompt("Voulez-vous des pepperoni : oui ou non");
 sauce = prompt("Voulez-vous de la sauce : oui ou non");
-nbPersonnes = prompt("Vous êtes combien de personne ?");
 
 // Légumes
 if(legume.toUpperCase() === "OUI"){
@@ -33,6 +31,7 @@ else {
 }
 // sauce
 if(sauce.toUpperCase() ==="OUI"){
+    nbPersonnes = prompt("Vous êtes combien de personne ?");
     prix+=(1 * nbPersonnes);
     sauceTexte = "avec sauce. ";
 }
@@ -42,7 +41,7 @@ else{
 
 prix+=10; // + prix de base
 
-prixTotal = prix + (prix * taxesTotal);
+var prixTotal = prix + (prix * taxesTotal);
 
 document.write(legumeTexte + pepperonitexte + sauceTexte);
 document.write("Le prix avant taxes : " + prix + " $ ");
