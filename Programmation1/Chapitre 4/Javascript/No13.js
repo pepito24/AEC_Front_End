7eaa2d16ae632e70608536737fe8bb013133611d
pepito24 <erikpaquetweb@gmail.com>
2020-01-16 21:19:00 -0500

    2020

--- Programmation1/Chapitre 4/Javascript/No13.js
+++ Programmation1/Chapitre 4/Javascript/No13.js
@@ -7,7 +7,7 @@ for(var i = 0;i < 12;i++){
 }
 
 for (var j = 0;j < nombre.length;j++){
-    if( Math.max(nombre) + nombre[j] === 15 ){
+    if( nombre[11] + nombre[j] === 15 ){
         document.write(Math.max(nombre) + " et " + nombre[j])
     }
     else{
