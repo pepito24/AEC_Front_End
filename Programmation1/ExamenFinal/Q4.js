20d882ab6cf11626e665c8aa77dfcc0cb14db9ff
pepito24 <erikpaquetweb@gmail.com>
2020-02-07 13:52:28 -0500

    exercice

--- Programmation1/ExamenFinal/Q4.js
+++ Programmation1/ExamenFinal/Q4.js
@@ -7,25 +7,40 @@ class MonnaieVirtuelle {
         this.nomMonnaie = nomMonnaie;
         this.valeurEnUSD = valeurEnUSD;
         this.actif = actif;
-        this.actifUSD = this.actif * this.valeurEnUSD;
 
     }
+    actifUSD(){
+        return(this.actif * this.valeurEnUSD)
+}
 }
 
-class PorteFeuille {
+class portefeuille {
 
     constructor(nomProprietaire,tableauMonnaies) {
         this.nomProprietaire = nomProprietaire;
         this.tableauMonnaies = tableauMonnaies;
 
     }
-    ValeurDuPortefeuille(){s
-        return();
+    ValeurDuPortefeuille(){
+        let total = 0;
+        for(let i=0;i<tableauMonnaies.length;i++){
+            total += tableauMonnaies[i].actifUSD();
+        }
+        return total;
     }
+
+
 }
 
 let Monnaie1 = new MonnaieVirtuelle("Bitcoin", 9730,6);
 let Monnaie2 = new MonnaieVirtuelle("Ethereum", 194,20);
 let Monnaie3 = new MonnaieVirtuelle("Litecoin", 58,10);
 
-let personne = new PorteFeuille("Shany Carle");
+
+let tableauMonnaies= [Monnaie1,Monnaie2,Monnaie3];
+
+let personne = new portefeuille("Shany Carle",tableauMonnaies);
+
+let grandTotal= portefeuille.ValeurDuPortefeuille();
+
+document.write("<br>" + grandTotal + personne);
