e13e20d1a776c1f6873d3011824ac3ef2a9bf64e
pepito24 <erikpaquetweb@gmail.com>
2020-04-01 16:18:48 -0400

    2020

--- Programmation1/Examen2 JavaScript Photociti 2/Examen2 JavaScript Photociti 2/javascript.js
+++ Programmation1/Examen2 JavaScript Photociti 2/Examen2 JavaScript Photociti 2/javascript.js
@@ -1,4 +1,17 @@
 
+//mot
+var str = "";
+
+$("#btn").click(function(){
+    str = $("#inpt").val();
+    console.log(str.length);
+    //for(var j=0;j<=str.length;j++) {
+      //  console.log(str[j]);
+    //}
+    alert(str);
+});
+
+
 //changement fond
 $( "#red" ).click(function() {
     $('.section2').css("background-image" , "url(\"background/rouge.jpg\")");
@@ -22,26 +35,44 @@ $( "#blanc" ).click(function() {
     $('.section2').css("background-image" , "url(\"background/blanc.jpg\")");
 });
 
+
+
+///changer les images
 let premierClick = true;
+let modalShow = false;
 let idImage1;
 let idImage2;
 let srcImage1;
 let srcImage2;
 
-$('.img-fluid').click(function() {
+$('.lettre').click(function() {
     if(premierClick){
         idImage1 = $(this).attr("id");
         srcImage1 = $(this).attr("src");
         premierClick = false;
+        $('#modal').modal('show');
+        modalShow = true;
+        if (modalShow = true){
+            $('#img1').attr("src","Letters/" + (srcImage1[8]) + "/" + (srcImage1[8]) + "1.jpg");
+            $('#img2').attr("src","Letters/" + (srcImage1[8]) + "/" + (srcImage1[8]) + "2.jpg");
+            $('#img3').attr("src","Letters/" + (srcImage1[8]) + "/" + (srcImage1[8]) + "3.jpg");
+            $('#img4').attr("src","Letters/" + (srcImage1[8]) + "/" + (srcImage1[8]) + "4.jpg");
+            $('#img5').attr("src","Letters/" + (srcImage1[8]) + "/" + (srcImage1[8]) + "5.jpg");
+        }
+        modalShow = false;
     }
     else{
         idImage2 = $(this).attr("id");
         srcImage2 = $(this).attr("src");
-
+        $('#modal').modal('hide');
         $("#" + idImage1).attr("src",srcImage2);
         $("#" + idImage2).attr("src",srcImage1);
         premierClick = true;
-
     }
 
 });
+
+
+
+//for (var i=0;i<=5;i++){
+
