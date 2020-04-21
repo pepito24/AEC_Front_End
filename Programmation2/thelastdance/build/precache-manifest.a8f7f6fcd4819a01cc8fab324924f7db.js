32d586bbf7a42fba2a64dbd35c603ab6b2c895f7
pepito24 <erikpaquetweb@gmail.com>
2020-04-21 19:47:23 -0400

    1

---
+++ Programmation2/thelastdance/build/precache-manifest.a8f7f6fcd4819a01cc8fab324924f7db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e163daffa323dc55bcf828c438be9b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79fffcbf62c369c9ab15",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "acba0160f3b65756036f",
+    "url": "./static/js/2.8f6e9f04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8f6e9f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79fffcbf62c369c9ab15",
+    "url": "./static/js/main.80c0bf06.chunk.js"
+  },
+  {
+    "revision": "f14872e1fc8ecf217aa0",
+    "url": "./static/js/runtime-main.f66a7021.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
