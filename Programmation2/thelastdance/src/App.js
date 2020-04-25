a0aeec419934c1f88650c16cab4edd6b078cb327
pepito24 <erikpaquetweb@gmail.com>
2020-04-25 08:22:27 -0400

    2020

--- Programmation2/thelastdance/src/App.js
+++ Programmation2/thelastdance/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import logo from './netfix.png';
 import './App.css';
+import image from './1.jpg';
 
 
+const people = ['Steve Keer', 'Michael Jordan', 'Bill Wennington', 'Scottie Pippen', 'Dennis Rodman']; 
 
+const peopleLis = people.map((person,i) => 
+    <li key={'person' + i}>{person}</li>); 
+ 
 
+    let plat= Math.floor(Math.random() * 4 +1); 
+
+    const listeAlea= ( 
+      <ul> 
+        <li>Shany</li> 
+        { plat === 1 && <li>Pizza</li> } 
+        { plat === 2 && <li>Chinois</li> } 
+        { plat === 3 && <li>Thai</li> } 
+        { plat === 4 && <li>Vietnamien</li> } 
+      </ul> 
+    ); 
 
 function App() {
   return (  
@@ -17,6 +33,15 @@ function App() {
       <p>
          Mettant en vedette
       </p>
+      <h2>{peopleLis}</h2>
+      <img src={image} alt="logo" />
+      <p>
+      {peopleLis}
+      </p>
+      <p>
+      {listeAlea}
+      </p>
+
       </div>
     ); 
 }
