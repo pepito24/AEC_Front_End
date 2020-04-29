145b64582e5bf0ee0a61b97bad348026c38039ed
pepito24 <erikpaquetweb@gmail.com>
2020-04-28 20:05:19 -0400

    1

---
+++ Programmation2/becflix/build/precache-manifest.7d949f758428608b072df7edaae7ed04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56538e3d17d3326918f456ecdc4f1cbe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2e8b390e5541bc16e1ca",
+    "url": "./static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "f20497006bfc1d1bb5a6",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2e8b390e5541bc16e1ca",
+    "url": "./static/js/2.872c099b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.872c099b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f20497006bfc1d1bb5a6",
+    "url": "./static/js/main.eaa36730.chunk.js"
+  },
+  {
+    "revision": "ef8e272eb776f0edacee",
+    "url": "./static/js/runtime-main.03e9e90d.js"
+  },
+  {
+    "revision": "edcb37370b7753d01f6528680ed92efe",
+    "url": "./static/media/22.edcb3737.jpg"
+  }
+]);
---
+++ Programmation2/becflix/src/components/TvShow/TvShow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Col from 'react-bootstrap/Col';  
+
+import show from "../../images/22.jpg";
+const firstShow = <img id="img" src={show} alt="Vikings" />; 
+
+export class TvShow extends React.Component {  
+
+    render() {  
+       return <Col lg="2">{firstShow} </Col> ;  
+    }  
+};  
