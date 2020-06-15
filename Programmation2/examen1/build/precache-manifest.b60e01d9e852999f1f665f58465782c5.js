3e38311067e45bab1d8e46c3e49a6a616c8f77a5
pepito24 <erikpaquetweb@gmail.com>
2020-06-15 10:01:00 -0400

    fin

---
+++ Programmation2/examen1/build/precache-manifest.b60e01d9e852999f1f665f58465782c5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b1fd495e66a0a4f0ab2f289288fdcc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c0f1e3c2de4fb505728a",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cf48c27ff437201e4a82",
+    "url": "./static/css/main.fb74574b.chunk.css"
+  },
+  {
+    "revision": "c0f1e3c2de4fb505728a",
+    "url": "./static/js/2.f3a02852.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.f3a02852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf48c27ff437201e4a82",
+    "url": "./static/js/main.610207b5.chunk.js"
+  },
+  {
+    "revision": "a99c846ba159af4b53f5",
+    "url": "./static/js/runtime-main.068a4d3a.js"
+  },
+  {
+    "revision": "5d6b10ff85b4f8a3246493fbe8ac347e",
+    "url": "./static/media/holly1.5d6b10ff.jpg"
+  },
+  {
+    "revision": "27482046ee4562cbe1dbba993a4d4b5d",
+    "url": "./static/media/holly2.27482046.jpg"
+  },
+  {
+    "revision": "10b94d61d6cc2b6148f398d01a9a58f3",
+    "url": "./static/media/liking1.10b94d61.jpg"
+  },
+  {
+    "revision": "ce1672c2417ea770a4631c184a05faf8",
+    "url": "./static/media/link2.ce1672c2.jpg"
+  },
+  {
+    "revision": "eca2e38f49cfd7dd216b84347d8c3153",
+    "url": "./static/media/logo.eca2e38f.png"
+  },
+  {
+    "revision": "98c92201ad38a1a115a33fb660f3b274",
+    "url": "./static/media/twd.98c92201.jpg"
+  },
+  {
+    "revision": "24fb55ef4799cf39bc60349294212e2a",
+    "url": "./static/media/white.24fb55ef.jpg"
+  }
+]);
