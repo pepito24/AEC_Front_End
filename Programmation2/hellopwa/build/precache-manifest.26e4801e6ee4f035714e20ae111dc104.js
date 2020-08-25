ed3c67e7e6afa03b73084930963fee2749ee0a51
pepito24 <erikpaquetweb@gmail.com>
2020-08-25 19:09:23 -0400

    2

---
+++ Programmation2/hellopwa/build/precache-manifest.26e4801e6ee4f035714e20ae111dc104.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa802098d4d6be5e598c425c0117d82",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f41a4684b9bbbdbcc703",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ecb4ad1ab68630ab7818",
+    "url": "./static/js/2.e9604be9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e9604be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f41a4684b9bbbdbcc703",
+    "url": "./static/js/main.cb4f523d.chunk.js"
+  },
+  {
+    "revision": "596db6d2290197e71c87",
+    "url": "./static/js/runtime-main.cf705ba3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
