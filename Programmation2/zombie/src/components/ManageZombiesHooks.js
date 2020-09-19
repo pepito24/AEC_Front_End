3d6199f8e2b8c16720787ea2494491d3c9732e51
pepito24 <erikpaquetweb@gmail.com>
2020-09-19 08:17:56 -0400

    2020/09/19

--- Programmation2/zombie/src/components/ManageZombiesHooks.js
+++ Programmation2/zombie/src/components/ManageZombiesHooks.js
@@ -9,14 +9,16 @@ function ManageZombiesHooks(){
 
     return (
       <>
-      <Container fluid className="box1">
+      <canvas id="canvas-image-blending">
+      <Container fluid className="">
         <Header/>
           <Row className="text-center text-white align-items-center">
             <Col lg="12" className="mb-5">
-            <img className="image" src="https://www.stateofdecay.com/wp-content/themes/state-of-decay-franchise/dist/images/sod_3_logo.png"/>
+            <img alt="" className="image" src="https://www.stateofdecay.com/wp-content/themes/state-of-decay-franchise/dist/images/sod_3_logo.png"/>
             </Col>
           </Row> 
           </Container> 
+          </canvas>
           <Gallery/> 
       </>
     ); 
