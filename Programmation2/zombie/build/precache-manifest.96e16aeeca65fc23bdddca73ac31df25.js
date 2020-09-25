42fa4fd6edaf039c44cc7bfc140ab576cf2ce615
pepito24 <erikpaquetweb@gmail.com>
2020-09-25 16:55:20 -0400

    7777

---
+++ Programmation2/zombie/build/precache-manifest.96e16aeeca65fc23bdddca73ac31df25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ffefb684dd3290bb42142189002c33",
+    "url": "./index.html"
+  },
+  {
+    "revision": "401f8fb4e824b7e61f64",
+    "url": "./static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "5d07eb9efaee88cd7a4d",
+    "url": "./static/css/main.3185eb29.chunk.css"
+  },
+  {
+    "revision": "401f8fb4e824b7e61f64",
+    "url": "./static/js/2.96ad20a7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.96ad20a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d07eb9efaee88cd7a4d",
+    "url": "./static/js/main.58e08f33.chunk.js"
+  },
+  {
+    "revision": "822f9654430bbcb3f8a5",
+    "url": "./static/js/runtime-main.be7ad75d.js"
+  }
+]);
--- Programmation2/zombie/build/service-worker.js
+++ Programmation2/zombie/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9bf4e9609c56f1319520182bcb06f54e.js"
+  "./precache-manifest.96e16aeeca65fc23bdddca73ac31df25.js"
 );
 
 self.addEventListener('message', (event) => {
