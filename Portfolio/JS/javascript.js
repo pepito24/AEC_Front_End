bc166275f26dad81eee2a3466731719143e58c6e
pepito24 <erikpaquetweb@gmail.com>
2020-11-04 08:51:39 -0500

    angular

---
+++ Portfolio/JS/javascript.js
@@ -0,0 +1,20 @@
+
+function github() {
+  window.open("https://github.com/pepito24");
+}
+
+function email(){
+  window.location.href = "mailto:erikpaquetweb@gmail.com";
+}
+
+function linkedin() {
+  window.open("https://www.linkedin.com/in/%C3%A9rik-paquet/");
+}
+
+function facebook() {
+  window.open("https://www.facebook.com/eric.paquet3/");
+}
+
+function insta() {
+  window.open("https://www.instagram.com/erik_lodbrok/");
+}
